f377bef11f7b41f612661b066175f33918f3bad8
Harry McCormick <148074236+HazWorld@users.noreply.github.com>
2025-09-06 14:32:20 +0100

    fixes to culture night

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import React from "react";
 import './App.css';
-import PosterBackground from "./images/RAW-tickets.svg";
+import PosterBackground from "./images/RAW-poster.png";
 import ticketButtonPng from "./images/TicketImage.png";
 
 export default function RetroEventPage() {
   return (
     <div className="App">
+      <div className="background-blur" />
+
+      {/* Poster with button overlay */}
       <div className="poster-wrapper">
         <img
           src={PosterBackground}
           alt="Background Poster"
-          className="background-svg"
+          className="poster-image"
         />
 
-        {/* Glitchable Ticket Button */}
-        <a
-          href="https://your-ticket-link.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="ticket-button"
-        >
-          <div className="ticket-button-glitch">
-            <img src={ticketButtonPng} alt="Buy Tickets" />
-          </div>
-        </a>
+        {/* Ticket button relative to image */}
+        <div className="ticket-button-overlay">
+          <a
+            href="https://www.paypal.com/ncp/payment/FY6F6Z34V2TKL"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ticket-button"
+          >
+            <div className="ticket-button-glitch">
+              <img src={ticketButtonPng} alt="Buy Tickets" />
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
